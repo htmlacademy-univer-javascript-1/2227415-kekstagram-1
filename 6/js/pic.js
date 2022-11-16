a9d5bade7920f958d312b7bb83d23424ca593c7e
Keks <keks@htmlacademy.ru>
2022-11-16 23:27:06 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/pic.js
@@ -0,0 +1,19 @@
+import {createPost} from './data.js';
+
+//Ищем шаблон по id
+const photosTemplate = document.querySelector('#picture').content;
+//Контейнер фото других пользователей
+const picsList = document.querySelector('.pictures');
+
+const postImg = createPost();
+const picFragment = document.createDocumentFragment();
+
+postImg.forEach(({url, likes, comments}) => {
+  const onePhoto = photosTemplate.cloneNode(true); //клонируем узел
+  onePhoto.querySelector('.picture__img').src = url;
+  onePhoto.querySelector('.picture__likes').textContent = likes;
+  onePhoto.querySelector('.picture__comments').textContent = comments;
+  picFragment.appendChild(onePhoto);
+});
+picsList.appendChild(picFragment);
+
